0b5a153139d4dcd6567cb9426e583d5479563ac1
Gabriel Domínguez <gdominguezreyes@danielcastelao.org>
2021-03-09 19:32:58 +0100

    Añadimos metodos clase principal

--- src/com/gabriel/Main.java
+++ src/com/gabriel/Main.java
@@ -1,11 +1,16 @@
 package com.gabriel;
 
-
+import LibreriaCOD.FactoriaDeSalidas;
+import LibreriaCOD.InterfaceMetodosComunes;
 
 public class Main {
 
     public static void main(String[] args) {
+        InterfaceMetodosComunes unaConsola = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.CONSOLA, "Hola");
+        unaConsola.visualiza();
 
+        InterfaceMetodosComunes unaVentana = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.VENTANA,"Chao");
+        unaVentana.visualiza();
 
 
     }
